c01a096b7092062da5ecb45226d87bbd7ea2daef
Leo Saavedra <saavedraisuizaleo7@gmail.com>
2025-09-02 09:23:51 -0500

    importe cors en el index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import { PORT } from "./config.js";
 import userRoutes from "./routes/users.routes.js";
-import morgan from 'morgan'
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 app.use(morgan('dev'))
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use(userRoutes);
 
 app.listen(PORT)
